76a5ac364e56a0c6205c597fece888768253ba00
pecca86 <pekka.rantaaho86@gmail.com>
2022-12-01 22:37:13 +0100

    Removed unused import

--- src/main/java/year2021/day1/DepthCalculator.java
+++ src/main/java/year2021/day1/DepthCalculator.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 import java.util.function.Function;
-import java.util.stream.IntStream;
 
 public class DepthCalculator {
     public static void main(String[] args) {
