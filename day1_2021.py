477f702d3391747203e36e0cf2de446478eaf658
pecca86 <pekka.rantaaho86@gmail.com>
2022-12-01 22:57:55 +0100

    2021 day 1 problem 1& 2 in python

---
+++ day1_2021.py
@@ -0,0 +1,26 @@
+
+
+with open('input.txt', 'r') as file:
+    times_increased = 0
+    increased_three_slide = 0
+    old_val = 99999
+    lines = file.readlines()
+
+    # Problem 1
+    for line in lines:
+        if int(line) > old_val:
+            times_increased += 1
+        old_val = int(line)
+
+    # Problem 2
+    for i in range(len(lines)):
+        try:
+            if (lines[i] < lines[i+3]):
+                increased_three_slide += 1
+        except IndexError:
+            break
+    
+print(lines[0])
+
+print(times_increased)
+print(increased_three_slide)
