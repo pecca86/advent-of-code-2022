d9f4bdcde498d193111fd2dc8f940dfc4e2e3a74
pecca86 <pekka.rantaaho86@gmail.com>
2022-12-01 14:19:37 +0100

    Python version of the solution

---
+++ day1.py
@@ -0,0 +1,15 @@
+with open('calories.txt', 'r') as file:
+    totalCals = 0
+    caloriesPerElf = []
+    lines = file.readlines()
+    for line in lines:
+        if line.startswith("\n"):
+            caloriesPerElf.append(totalCals)
+            totalCals = 0
+        else:
+            totalCals += int(line)
+
+    caloriesPerElf.sort()
+    print(caloriesPerElf[-1])
+    print(sum(tuple(caloriesPerElf[-3::])))
+    
