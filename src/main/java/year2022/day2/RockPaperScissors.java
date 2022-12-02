9790bd0f048bc0db5fce72afea71da963c612bf3
pecca86 <pekka.rantaaho86@gmail.com>
2022-12-02 12:16:11 +0100

    Day 2 Challenge 2, 2022.

--- src/main/java/year2022/day2/RockPaperScissors.java
+++ src/main/java/year2022/day2/RockPaperScissors.java
@@ -9,6 +9,7 @@ public class RockPaperScissors {
     private static final int LOSS = 0;
     private static final int DRAW = 3;
     private static final int WIN = 6;
+    public static final String FILE_PATH = "src/main/java/year2022/day2/input.txt";
 
     private static List<String> elfSequence;
     private static List<String> humanSequence;
@@ -36,6 +37,9 @@ public class RockPaperScissors {
 
         int humanPoints = getHumanPoints();
         System.out.println(humanPoints);
+
+        int secondFormulaResult = getHumanPointSecondChallenge();
+        System.out.println(secondFormulaResult);
     }
 
     private static int getHumanPoints() {
@@ -60,23 +64,54 @@ public class RockPaperScissors {
         return humanPoints;
     }
 
+    /**
+     * X = Lose
+     * Y = Draw
+     * Z = Win
+     * @return total points
+     */
+    private static int getHumanPointSecondChallenge() {
+        int humanPoints = 0;
+        for (int i = 0; i < humanSequence.size(); i++) {
+            String elfHand = Hands.valueOf(elfSequence.get(i)).hand;
+            String humanHand = Hands.valueOf(humanSequence.get(i)).hand;
+
+            if (humanHand.equals(Hands.X.hand)) {
+                humanPoints += elfHand.equals(Hands.A.hand) ? Hands.C.points + LOSS : 0;
+                humanPoints += elfHand.equals(Hands.B.hand) ? Hands.A.points + LOSS : 0;
+                humanPoints += elfHand.equals(Hands.C.hand) ? Hands.B.points + LOSS : 0;
+            } else if (humanHand.equals(Hands.Y.hand)) {
+                humanPoints += elfHand.equals(Hands.A.hand) ? Hands.A.points + DRAW : 0;
+                humanPoints += elfHand.equals(Hands.B.hand) ? Hands.B.points + DRAW : 0;
+                humanPoints += elfHand.equals(Hands.C.hand) ? Hands.C.points + DRAW : 0;
+            } else {
+                humanPoints += elfHand.equals(Hands.A.hand) ? Hands.B.points + WIN : 0;
+                humanPoints += elfHand.equals(Hands.B.hand) ? Hands.C.points + WIN : 0;
+                humanPoints += elfHand.equals(Hands.C.hand) ? Hands.A.points + WIN : 0;
+            }
+        }
+        return humanPoints;
+    }
+
     private static void readInputFileToLists() {
         elfSequence = new ArrayList<>();
         humanSequence = new ArrayList<>();
+        String[] splitted;
 
-        try (Scanner input = openFile("src/main/java/year2022/day2/input.txt")) {
+        try (Scanner input = openFile()) {
             while (input.hasNextLine()) {
                 String line = input.nextLine();
-                elfSequence.add(line.split(" ")[0]);
-                humanSequence.add(line.split(" ")[1]);
+                splitted = line.split(" ");
+                elfSequence.add(splitted[0]);
+                humanSequence.add(splitted[1]);
             }
         } catch (NumberFormatException | FileNotFoundException e) {
             e.printStackTrace();
         }
     }
 
-    private static Scanner openFile(String filePath) throws FileNotFoundException {
-        return new Scanner(new BufferedReader(new FileReader(filePath)));
+    private static Scanner openFile() throws FileNotFoundException {
+        return new Scanner(new BufferedReader(new FileReader(FILE_PATH)));
     }
 }
 
