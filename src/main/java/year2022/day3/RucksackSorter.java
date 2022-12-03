0c252ce7aa5ae7e84d11b85870c10accac0913f2
pecca86 <pekka.rantaaho86@gmail.com>
2022-12-03 18:53:04 +0100

    Rearranged the code blocks.

--- src/main/java/year2022/day3/RucksackSorter.java
+++ src/main/java/year2022/day3/RucksackSorter.java
@@ -42,43 +42,30 @@ public class RucksackSorter {
         System.out.println("Result of problem no.2: " + finalResult);
     }
 
-    private static void calculatePointsToSecondChallengeList() {
-        groupBadgeItem
-                .forEach(e -> secondChallengePointList.add(charToPointsMap.get(e)));
-    }
-
-    private static void processGroupsToFindCommonItem() {
-        groupMap.values()
-                .forEach(RucksackSorter::insertCommonGroupItemToList);
-    }
-
-    private static void insertCommonGroupItemToList(List<String> group) {
-        String intermediateResult = parseOutAllDifferencesFromBothHalves(group.get(0), group.get(1));
-        String result = parseOutAllDifferencesFromBothHalves(intermediateResult, group.get(2));
-        groupBadgeItem.add(result.charAt(0));
-    }
-
-    private static void generateGroupMap() {
-        List<String> group = new ArrayList<>();
-        int groupNumber = 0;
-        for (int i = 0; i < rucksacks.size(); i++) {
-            if (i > 1 && i % 3 == 0) {
-                groupMap.put(groupNumber, group);
-                group = new ArrayList<>();
-                groupNumber++;
+    private static void readInputFileToLists() {
+        try (Scanner input = openFile()) {
+            while (input.hasNextLine()) {
+                rucksacks.add(input.nextLine());
             }
-            group.add(rucksacks.get(i));
-        }
-
-        if (!group.isEmpty()) {
-            groupMap.put(groupNumber, group);
+        } catch (NumberFormatException | FileNotFoundException e) {
+            e.printStackTrace();
         }
     }
 
-    private static int calculatePoints() {
-        return pointList.stream()
-                .mapToInt(Integer::intValue)
-                .sum();
+    private static Scanner openFile() throws FileNotFoundException {
+        return new Scanner(new BufferedReader(new FileReader(FILE_PATH)));
+    }
+
+    private static void initializeCharacterMaps() {
+        charToPointsMap = new HashMap<>();
+
+        List<Character> lowerCaseAlphabet = Arrays.stream("abcdefghijklmnopqrstuvwxyz".split("")).map(e -> e.charAt(0)).toList();
+        List<Character> upperCaseAlphabet = Arrays.stream("abcdefghijklmnopqrstuvwxyz".toUpperCase().split("")).map(e -> e.charAt(0)).toList();
+
+        AtomicInteger counter = new AtomicInteger(1);
+        lowerCaseAlphabet.forEach(character -> charToPointsMap.put(character, counter.getAndIncrement()));
+        counter.set(27);
+        upperCaseAlphabet.forEach(character -> charToPointsMap.put(character, counter.getAndIncrement()));
     }
 
     private static void processRucksackList() {
@@ -98,12 +85,6 @@ public class RucksackSorter {
         }
     }
 
-    @Contract(pure = true)
-    private static @NotNull String parseOutAllDifferencesFromBothHalves(@NotNull String firstHalf, @NotNull String secondHalf) {
-        String regex = "[^"+ secondHalf +"]";
-        return firstHalf.replaceAll(regex, "");
-    }
-
     private static @NotNull List<String> splitString(@NotNull String targetString) {
         List<String> result = new ArrayList<>();
         result.add(targetString.substring(0,targetString.length() / 2));
@@ -111,29 +92,48 @@ public class RucksackSorter {
         return result;
     }
 
-    private static void initializeCharacterMaps() {
-        charToPointsMap = new HashMap<>();
-
-        List<Character> lowerCaseAlphabet = Arrays.stream("abcdefghijklmnopqrstuvwxyz".split("")).map(e -> e.charAt(0)).toList();
-        List<Character> upperCaseAlphabet = Arrays.stream("abcdefghijklmnopqrstuvwxyz".toUpperCase().split("")).map(e -> e.charAt(0)).toList();
-
-        AtomicInteger counter = new AtomicInteger(1);
-        lowerCaseAlphabet.forEach(character -> charToPointsMap.put(character, counter.getAndIncrement()));
-        counter.set(27);
-        upperCaseAlphabet.forEach(character -> charToPointsMap.put(character, counter.getAndIncrement()));
+    private static int calculatePoints() {
+        return pointList.stream()
+                .mapToInt(Integer::intValue)
+                .sum();
     }
 
-    private static void readInputFileToLists() {
-        try (Scanner input = openFile()) {
-            while (input.hasNextLine()) {
-                rucksacks.add(input.nextLine());
+    private static void insertCommonGroupItemToList(List<String> group) {
+        String intermediateResult = parseOutAllDifferencesFromBothHalves(group.get(0), group.get(1));
+        String result = parseOutAllDifferencesFromBothHalves(intermediateResult, group.get(2));
+        groupBadgeItem.add(result.charAt(0));
+    }
+
+    @Contract(pure = true)
+    private static @NotNull String parseOutAllDifferencesFromBothHalves(@NotNull String firstHalf, @NotNull String secondHalf) {
+        String regex = "[^"+ secondHalf +"]";
+        return firstHalf.replaceAll(regex, "");
+    }
+
+    private static void generateGroupMap() {
+        List<String> group = new ArrayList<>();
+        int groupNumber = 0;
+        for (int i = 0; i < rucksacks.size(); i++) {
+            if (i > 1 && i % 3 == 0) {
+                groupMap.put(groupNumber, group);
+                group = new ArrayList<>();
+                groupNumber++;
             }
-        } catch (NumberFormatException | FileNotFoundException e) {
-            e.printStackTrace();
+            group.add(rucksacks.get(i));
+        }
+
+        if (!group.isEmpty()) {
+            groupMap.put(groupNumber, group);
         }
     }
 
-    private static Scanner openFile() throws FileNotFoundException {
-        return new Scanner(new BufferedReader(new FileReader(FILE_PATH)));
+    private static void processGroupsToFindCommonItem() {
+        groupMap.values()
+                .forEach(RucksackSorter::insertCommonGroupItemToList);
+    }
+
+    private static void calculatePointsToSecondChallengeList() {
+        groupBadgeItem
+                .forEach(e -> secondChallengePointList.add(charToPointsMap.get(e)));
     }
 }
